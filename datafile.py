7487f5878aca13f95429caa5b35690aa558afffd
ryanlharper <ryanlharper@gmail.com>
2024-01-06 08:08:16 -0500

    changes to files

---
+++ datafile.py
@@ -0,0 +1,11 @@
+# add code here
+import pandas as pd
+
+data = {
+        'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+        'Age': [25, 30, 35, 40],
+        'City': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']
+    }
+
+# Creating a DataFrame
+df = pd.DataFrame(data)
